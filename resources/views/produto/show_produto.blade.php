3c4059b359fb5b7f25e4454a8a8cf02135c5bb93
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2023-09-27 10:20:26 -0300

    o css que está na codigo da view foi colocado em um arquivo separado

--- resources/views/produto/show_produto.blade.php
+++ resources/views/produto/show_produto.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.template_view')
 
 @section('insert_head')
-    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/show_produto.css') }}">
 @endsection
 
 @section('insert_body')
@@ -12,148 +12,113 @@
     </header>
 
     <main>
-        <div style="
-                margin: 20px;
-                width: 1180px;
-                background-color: white;
-                border-radius: 4px;
-                display: flex">
-
-            {{-- {{dd($produto)}} --}}
-
-            <div style="width: 35%; margin-top:40px; display: flex;"> {{-- <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}"> --}}
-                <div style="padding-left: 12px; display:flex; flex-direction:column; gap:15px;">
+        <div class="container-product">
+            <div class="card-img-product">
+                <div class="container-imgs-small">
                     @if (isset($produto->imagem_01))
-                        <div style="border: 1px solid rgba(0,0,0,.4); border-radius: 3px;width: 54px; cursor:pointer;">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}" style="width: 100%">
+                        <div class="img-small">
+                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}">
                         </div>
                     @endif
 
                     @if (isset($produto->imagem_02))
-                        <div style="border: 1px solid rgba(0,0,0,.4); border-radius: 3px;width: 54px; cursor:pointer;">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_02}}" style="width: 100%">
+                        <div class="img-small">
+                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_02}}">
                         </div>
                     @endif
 
                     @if (isset($produto->imagem_03))
-                        <div style="border: 1px solid rgba(0,0,0,.4); border-radius: 3px;width: 54px; cursor:pointer;">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_03}}" style="width: 100%">
+                        <div class="img-small">
+                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_03}}">
                         </div>
                     @endif
 
                     @if (isset($produto->imagem_04))
-                        <div style="border: 1px solid rgba(0,0,0,.4); border-radius: 3px;width: 54px; cursor:pointer;">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_04}}" style="width: 100%">
+                        <div class="img-small">
+                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_04}}">
                         </div>
                     @endif
 
                     @if (isset($produto->imagem_05))
-                        <div style="border: 1px solid rgba(0,0,0,.4); border-radius: 3px;width: 54px; cursor:pointer;">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_05}}" style="width: 100%">
+                        <div class="img-small">
+                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_05}}">
                         </div>
                     @endif
                 </div>
 
-
-                <div style="margin-top: 16px;">
-                    <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}" style="width: 100%">
+                <div class="img-product">
+                    <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}">
                 </div>
             </div>
 
 
+            <div class="container-description-product">
+                <h1>{{$produto->nome}}</h1>
 
-            <div style="width: 33%; padding-left: 30px; margin-top: 84px">
-                <h1
-                    style="
-                        color: rgba(0,0,0,.9);
-                        font-size:22px;">{{$produto->nome}}
-                </h1>
-
-                <div style="margin-top: 36px; margin-bottom: 6px">
-                    <span style="color: rgba(0,0,0,.55); text-decoration-line: line-through;">R$ {{$produto->preco}}</span>
+                <div class="card-information-product">
+                    <span class="price-product">R$ {{$produto->preco}}</span>
                     <div>
-                        <span style="font-size: 32px">R$ {{ number_format(($produto->preco_desconto),0,',','.')}}</span>
-                        <span  style="font-size:18px; font-weight: 500; color: #00a650;">{{$produto->desconto}}% OFF</span>
+                        <span class="discount-price">R$ {{ number_format(($produto->preco_desconto),0,',','.')}}</span>
+                        <span  class="discount">{{$produto->desconto}}% OFF</span>
                     </div>
-                    <span style="font-size:18px; font-weight: 500; color: #00a650;">10x R$ {{ number_format(($produto->preco_desconto/10),2,',','.')}} sem juros</span>
+
+                    <span class="installment">10x R$ {{ number_format(($produto->preco_desconto/10),2,',','.')}} sem juros</span>
                 </div>
 
-                <a href="" style="text-decoration: none; color: #2968c8; font-size: 14px;">Ver os meios e pagamento</a>
+                <a href="">Ver os meios e pagamento</a>
 
-                <div style="
-                    font-size: 12px;
-                    background-color: #3483fa;
-                    margin-top: 8px;
-                    width: 100px;
-                    text-align: center;
-                    border-radius: 3px;
-                    color: #FFF;
-                    padding: 2px;">
+                <div class="offer-day">
                     OFERTA DO DIA
                 </div>
             </div>
 
 
-            <div style="width: 32%; padding: 16px">
-                <div style="
-                    border: 1px solid rgba(0,0,0,.1);
-                    border-radius: 10px;
-                    width: 100%;
-                    padding: 44px 14px ">
-
+            <div class="container-details">
+                <div class="card-details">
                     {{-- <p style="font-size: 16px; color: #00a650; font-weight: 600;">Frete grátis</p> --}}
+                    <p class="stock">Estoque disponível</p>
+                    <p class="description-stored">
+                        Armazenado e enviado pelo<span><i class="fa-solid fa-bolt-lightning"></i> FULL</span>
+                    </p>
 
-                    <p style="font-size: 16px; font-weight: 500">Estoque disponível</p>
-                    <p style="color: rgba(0,0,0,.55); font-size: 14px">Armazenado e enviado pelo <span style="font-size:12px; color: #00a650; font-weight: 900; font-style:italic"><i class="fa-solid fa-bolt-lightning"></i> FULL</span> </p>
-
-                    <div style="margin: 14px 0; font-size: 16px">Quantidade: 1 unidade
-                        <span style="font-size: 12px; color: rgba(0,0,0,.55);">({{$produto->estoque}} disponíveis)</span>
+                    <div class="available-stock">Quantidade: 1 unidade
+                        <span>({{$produto->estoque}} disponíveis)</span>
                     </div>
-
 
                     <div>
                         <a href="">
-                            <button style="border-radius: 8px; font-weight: 600; height:48px">
-                                Comprar
-                            </button>
+                            <button class="call-action">Comprar</button>
                         </a>
 
-                        <button style="background-color:rgba(65,137,230,.15); color: #3483fa; border-radius: 8px; font-weight: 600; height:48px">
+                        <button class="add-cart">
                             Adicionar ao carrinho
                         </button>
                     </div>
 
-                    <div style="display:flex; gap:7px; margin-top: 10px">
+                    <div class="card-information-buy">
                         <div>
                             <i class="fa-solid fa-share fa-rotate-180" style="color: rgba(0,0,0,.55);"></i>
                         </div>
 
-                        <div style="display: flex; flex-direction:column">
-                            <a href="" style="font-size: 14px; color: #3483fa; text-decoration: none">Devolução grátis.</a>
-                            <span style="color: rgba(0,0,0,.55); font-size: 14px">Você tem 7 dias a partir da data de recebimento.</span>
-
+                        <div class="information-about">
+                            <a href="">Devolução grátis.</a>
+                            <span>Você tem 7 dias a partir da data de recebimento.</span>
                         </div>
                     </div>
 
-                    <div style="display:flex; gap:7px; margin-top: 10px">
+                    <div class="card-information-buy">
                         <div>
                            <i class="fa-solid fa-shield-halved" style="color: rgba(0,0,0,.55);"></i>
                         </div>
 
-                        <div style="display: flex; flex-direction:column">
-                            <a href="" style="font-size: 14px; color: #3483fa; text-decoration: none">Compra Garantida.</a>
-                            <span style="color: rgba(0,0,0,.55); font-size: 14px">receba o produto que está esperando ou devolvemos o dinheiro.</span>
+                        <div class="information-about">
+                            <a href="">Compra Garantida.</a>
+                            <span>receba o produto que está esperando ou devolvemos o dinheiro.</span>
                         </div>
                     </div>
-
-
                 </div>
-
-
-
             </div>
 
         </div>
-
     </main>
 @endsection
