76c45588b4f4d000d3c19d46f1ba575cd5a837ec
Anthony Aragao <anthony.amoz.s.a@gmail.com>
2023-09-28 22:28:38 -0300

    o produto pode ser exibido caso o usuario nao esteja autenticado

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('produto/{id}', [ProdutoController::class, 'show'])->name('produto.show');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
