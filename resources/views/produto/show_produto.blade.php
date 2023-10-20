db75a66571e4906f907406e120e639e689c77d89
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2023-10-20 13:22:11 -0300

    avaliações adicionadas ao exibir o produto

--- resources/views/produto/show_produto.blade.php
+++ resources/views/produto/show_produto.blade.php
@@ -39,109 +39,156 @@
 
     <main>
         <div class="container-product">
-            <div class="card-img-product">
-                <div class="container-imgs-small">
-                    @if (isset($produto->imagem_01))
-                        <div class="img-small border-blue">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}">
-                        </div>
-                    @endif
+            <div style="display: flex">
+                <div class="card-img-product">
+                    <div class="container-imgs-small">
+                        @if (isset($produto->imagem_01))
+                            <div class="img-small border-blue">
+                                <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}">
+                            </div>
+                        @endif
 
-                    @if (isset($produto->imagem_02))
-                        <div class="img-small">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_02}}">
-                        </div>
-                    @endif
+                        @if (isset($produto->imagem_02))
+                            <div class="img-small">
+                                <img src="{{ asset('files/produtos')}}/{{$produto->imagem_02}}">
+                            </div>
+                        @endif
 
-                    @if (isset($produto->imagem_03))
-                        <div class="img-small">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_03}}">
-                        </div>
-                    @endif
+                        @if (isset($produto->imagem_03))
+                            <div class="img-small">
+                                <img src="{{ asset('files/produtos')}}/{{$produto->imagem_03}}">
+                            </div>
+                        @endif
 
-                    @if (isset($produto->imagem_04))
-                        <div class="img-small">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_04}}">
-                        </div>
-                    @endif
+                        @if (isset($produto->imagem_04))
+                            <div class="img-small">
+                                <img src="{{ asset('files/produtos')}}/{{$produto->imagem_04}}">
+                            </div>
+                        @endif
 
-                    @if (isset($produto->imagem_05))
-                        <div class="img-small">
-                            <img src="{{ asset('files/produtos')}}/{{$produto->imagem_05}}">
-                        </div>
-                    @endif
-                </div>
-
-                <div class="img-product">
-                    <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}">
-                </div>
-            </div>
-
-            <div class="container-description-product">
-                <h1>{{$produto->nome}}</h1>
-
-                <div class="card-information-product">
-                    <span class="price-product">R$ {{ number_format(($produto->preco),2,',','.')}} </span>
-                    <div>
-                        <span class="discount-price">R$ {{ number_format(($produto->preco_desconto),0,',','.')}}</span>
-                        <span  class="discount">{{$produto->desconto}}% OFF</span>
+                        @if (isset($produto->imagem_05))
+                            <div class="img-small">
+                                <img src="{{ asset('files/produtos')}}/{{$produto->imagem_05}}">
+                            </div>
+                        @endif
                     </div>
 
-                    <span class="installment">10x R$ {{ number_format(($produto->preco_desconto/10),2,',','.')}} sem juros</span>
+                    <div class="img-product">
+                        <img src="{{ asset('files/produtos')}}/{{$produto->imagem_01}}">
+                    </div>
                 </div>
 
-                <a href="">Ver os meios e pagamento</a>
+                <div class="container-description-product">
+                    <h1>{{$produto->nome}}</h1>
 
-                <div class="offer-day">
-                    OFERTA DO DIA
-                </div>
-            </div>
-
-            <div class="container-details">
-                <div class="card-details">
-                    {{-- <p style="font-size: 16px; color: #00a650; font-weight: 600;">Frete grátis</p> --}}
-                    <p class="stock">Estoque disponível</p>
-                    <p class="description-stored">
-                        Armazenado e enviado pelo <span><i class="fa-solid fa-bolt-lightning"></i> FULL</span>
-                    </p>
-
-                    <div class="available-stock">Quantidade: 1 unidade
-                        <span>({{$produto->estoque}} disponíveis)</span>
-                    </div>
-
-                    <div>
-                        <a href="">
-                            <button class="btn call-action">Comprar</button>
-                        </a>
-
-                        <button class="btn add-cart">
-                            Adicionar ao carrinho
-                        </button>
-                    </div>
-
-                    <div class="card-information-buy">
+                    <div class="card-information-product">
+                        <span class="price-product">R$ {{ number_format(($produto->preco),2,',','.')}} </span>
                         <div>
-                            <i class="fa-solid fa-share fa-rotate-180" style="color: rgba(0,0,0,.55);"></i>
+                            <span class="discount-price">R$ {{ number_format(($produto->preco_desconto),0,',','.')}}</span>
+                            <span  class="discount">{{$produto->desconto}}% OFF</span>
                         </div>
 
-                        <div class="information-about">
-                            <a href="">Devolução grátis.</a>
-                            <span>Você tem 7 dias a partir da data de recebimento.</span>
-                        </div>
+                        <span class="installment">10x R$ {{ number_format(($produto->preco_desconto/10),2,',','.')}} sem juros</span>
                     </div>
 
-                    <div class="card-information-buy">
-                        <div>
-                           <i class="fa-solid fa-shield-halved" style="color: rgba(0,0,0,.55);"></i>
+                    <a href="">Ver os meios e pagamento</a>
+
+                    <div class="offer-day">
+                        OFERTA DO DIA
+                    </div>
+                </div>
+
+                <div class="container-details">
+                    <div class="card-details">
+                        {{-- <p style="font-size: 16px; color: #00a650; font-weight: 600;">Frete grátis</p> --}}
+                        <p class="stock">Estoque disponível</p>
+                        <p class="description-stored">
+                            Armazenado e enviado pelo <span><i class="fa-solid fa-bolt-lightning"></i> FULL</span>
+                        </p>
+
+                        <div class="available-stock">Quantidade: 1 unidade
+                            <span>({{$produto->estoque}} disponíveis)</span>
                         </div>
 
-                        <div class="information-about">
-                            <a href="">Compra Garantida.</a>
-                            <span>receba o produto que está esperando ou devolvemos o dinheiro.</span>
+                        <div>
+                            <a href="">
+                                <button class="btn call-action">Comprar</button>
+                            </a>
+
+                            <button class="btn add-cart">
+                                Adicionar ao carrinho
+                            </button>
+                        </div>
+
+                        <div class="card-information-buy">
+                            <div>
+                                <i class="fa-solid fa-share fa-rotate-180" style="color: rgba(0,0,0,.55);"></i>
+                            </div>
+
+                            <div class="information-about">
+                                <a href="">Devolução grátis.</a>
+                                <span>Você tem 7 dias a partir da data de recebimento.</span>
+                            </div>
+                        </div>
+
+                        <div class="card-information-buy">
+                            <div>
+                            <i class="fa-solid fa-shield-halved" style="color: rgba(0,0,0,.55);"></i>
+                            </div>
+
+                            <div class="information-about">
+                                <a href="">Compra Garantida.</a>
+                                <span>receba o produto que está esperando ou devolvemos o dinheiro.</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            @if (count($avaliacoes) !== 0)
+                <div class="line" style="margin:70px 0"></div>
+
+                <div class="container-assessment-external">
+                    <div class="container-assessment-internal">
+                        <div>
+                            <h2 class="assessment-product">Opiniões do produto</h2>
+
+                            @foreach ($avaliacoes->take(5) as $avaliacao)
+                                <div class="line"></div>
+
+                                <div class="container-stars">
+                                    <div>
+                                        <i class="fa-solid fa-star fa-xs" style="color: rgb(52, 131, 250);"></i>
+                                        <i class="fa-solid fa-star fa-xs" style="color: rgb(52, 131, 250);"></i>
+                                        <i class="fa-solid fa-star fa-xs" style="color: rgb(52, 131, 250);"></i>
+                                        <i class="fa-solid fa-star fa-xs" style="color: rgb(52, 131, 250);"></i>
+                                        <i class="fa-solid fa-star fa-xs" style="color: rgb(52, 131, 250);"></i>
+                                    </div>
+
+                                    <div>
+                                        <span class="formatted-date">{{ \Carbon\Carbon::parse($avaliacao->data)->format('d M. Y') }}</span>
+                                    </div>
+                                </div>
+
+                                <p class="comment">
+                                    {{ $avaliacao->comentario }}
+                                </p>
+
+                                <div class="container-btns-feedback">
+                                    <div class="feedback-like">
+                                        <span>É útil <i class="fa-regular fa-thumbs-up"></i></span>
+                                    </div>
+
+                                    <div class="feedback-dislike">
+                                        <i class="fa-regular fa-thumbs-up fa-flip-vertical"></i>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            @endif
+
 
         </div>
     </main>
