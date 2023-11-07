0694130f6c0512d57b6275ad5235952f5cf89193
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2023-11-07 10:07:32 -0300

    css adicionado em um arquivo separado

--- resources/views/pedidos/meus_pedidos.blade.php
+++ resources/views/pedidos/meus_pedidos.blade.php
@@ -3,7 +3,7 @@
 @section('insert_head')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/meu_pedidos.css') }}">
     <title>Mercado libre</title>
 @endsection
 
@@ -41,45 +41,22 @@
     </header>
 
     <main>
-        <div style="width: 1180px">
-            <h2 style="font-size: 26px; margin: 30px 0;">Compras</h2>
+        <div class="container">
+            <h2>Compras</h2>
 
-            <div style="
-                background-color: white;
-                border-radius: 4px;
-                margin-bottom:20px;
-                padding: 10px 30px;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;">
-                <span style="color: rgba(0,0,0,.55);"><i class="fa-solid fa-star"></i> Alguns produtos esperam sua opinião</span>
+            <div class="comment-block">
+                <span>
+                    <i class="fa-solid fa-star"></i> Alguns produtos esperam sua opinião
+                </span>
 
-                <button
-                    style="
-                    width: 160px;
-                    height: 38px;
-                    cursor: pointer;
-                    border: none;
-                    background-color:rgba(65,137,230,.15);
-                    transition: .5s;
-                    color: #3483fa !important;
-                    font-size: .9rem;
-                    margin-top: 10px;
-                    border-radius: 8px;
-                    font-weight: 600;">
+                <button class="btn" style="color: #3483fa;">
                     Opinar
                 </button>
             </div>
 
             @foreach ($compras as $compra)
-                <div style="
-                    background-color: white;
-                    border-radius: 4px;
-                    margin-bottom:20px">
-
-                    <div style="
-                    padding: 17px 0px 0px 30px;
-                    border-bottom: 1px solid #ededed;">
+                <div class="shopping">
+                    <div class="date">
                         <p>
                             <?php
                             $meses = array(
@@ -96,69 +73,36 @@
                         </p>
                     </div>
 
-                    <div style="
-                        padding: 30px;
-                        display: flex;
-                        gap: 6px;
-                        justify-content: space-between;">
-
-                        <div style="display: flex; gap:16px; width:40%;">
+                    <div class="details">
+                        <div class="details-img">
                             <div>
-                                <img src="{{ asset('files/produtos')}}/{{$compra->exemplar[0]->pivo->produto->imagem_01}}"
-                                style="
-                                    width: 70px;
-                                    height: 70px;
-                                    border: 1px solid #ededed;
-                                    padding: 2px;
-                                    border-radius: 4px;">
+                                <img src="{{ asset('files/produtos')}}/{{$compra->exemplar[0]->pivo->produto->imagem_01}}">
                             </div>
 
-                            <span style="font-size: 14px; color: rgba(0,0,0,.55);">
+                            <span>
                                 {{$compra->exemplar[0]->pivo->produto->nome}}
                             </span>
                         </div>
 
-                        <div style="width:30%; display: flex; flex-direction: column;">
-                            <span style="font-size: 14px; color: rgba(0,0,0,.55);">
+                        <div class="details-producer">
+                            <span>
                                 <?php echo strtoupper($compra->exemplar[0]->pivo->produtor->dados_empresa->nome); ?>
                             </span>
-                            <a href="" style="text-decoration: none; color: #3483fa">Enviar mensagem</a>
+
+                            <a href="">Enviar mensagem</a>
                         </div>
 
-                        <div style="width:15%; display:flex; flex-direction: column; gap:2px">
+                        <div class="container-btns">
                             <a href="">
-                                <button
-                                    style=" width: 100%;
-                                    height: 38px;
-                                    cursor: pointer;
-                                    border: none;
-                                    background-color: #3783f7;
-                                    transition: .5s;
-                                    color: white;
-                                    font-size: .9rem;
-                                    margin-top: 10px;
-                                    border-radius: 8px;
-                                    font-weight: 600;">
+                                <button class="btn" style="background-color: #3783f7; color: white;">
                                     Ver compra
                                 </button>
                             </a>
 
-                            <button
-                                style=" width: 100%;
-                                height: 38px;
-                                cursor: pointer;
-                                border: none;
-                                background-color:rgba(65,137,230,.15);
-                                transition: .5s;
-                                color: #3483fa !important;
-                                font-size: .9rem;
-                                margin-top: 10px;
-                                border-radius: 8px;
-                                font-weight: 600;">
+                            <button class="btn" style="width: 100%; background-color:rgba(65,137,230,.15); color: #3483fa">
                                 Comprar novamente
                             </button>
                         </div>
-
                     </div>
                 </div>
             @endforeach
