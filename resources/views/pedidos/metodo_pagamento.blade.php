c933749b45ad5e1dc1ea906882a29c6badfa2454
Anthony Aragao <anthony.amoz.s.a@gmail.com>
2024-02-12 23:56:47 -0300

    add do css em um arquivo separado

--- resources/views/pedidos/metodo_pagamento.blade.php
+++ resources/views/pedidos/metodo_pagamento.blade.php
@@ -19,141 +19,53 @@
 
     <main>
         <div class="container">
-            <div style="margin-top: 30px; width: 800px; display:flex; flex-direction: column;">
-                <h2 style="font-size: 18px; margin-bottom:30px">Como você prefere pagar?</h2>
+            <div class="methods-payments">
+                <h2>Como você prefere pagar?</h2>
 
-                <a href="{{ route('session', [Crypt::encrypt($produto->id), 'card']) }}"
-                    style="
-                        width: 100%;
-                        height:80px;
-                        background-color: #fff;
-                        cursor:pointer;
-                        text-decoration:none;
-                        display: flex;
-                        align-items: center;
-                        border: 1px solid #e0e0e0;
-                        padding: 0 30px;
-                        gap: 20px;"
-                    >
-
-                    <div style="height: 50px;  width:50px; background-color:#e0e0e0; border-radius:50%; display:flex;  justify-content: center; align-items: center">
+                <a href="{{ route('session', [Crypt::encrypt($produto->id), 'card']) }}" class="card-payment">
+                    <div class="img-method-payment">
                         <i class="fa-solid fa-credit-card" style="font-size: 24px;"></i>
                     </div>
 
-                    <div>
-                        <p style="font-size:14px; color:black">Comprar com Cartão</p>
-                    </div>
+                    <p>Comprar com Cartão</p>
                 </a>
 
-                <a href="{{ route('session', [Crypt::encrypt($produto->id), 'card']) }}"
-                    style="
-                        width: 100%;
-                        height:80px;
-                        background-color: #fff;
-                        cursor:pointer;
-                        text-decoration:none;
-                        display: flex;
-                        align-items: center;
-                        border: 1px solid #e0e0e0;
-                        padding: 0 30px;
-                        gap: 20px;"
-                    >
+                <span>Com outros meios de pagamento</span>
 
-                    <div style="height: 50px;  width:50px; background-color:#e0e0e0; border-radius:50%; display:flex;  justify-content: center; align-items: center">
-                        <i class="fa-solid fa-credit-card" style="font-size: 24px;"></i>
+                <a href="{{ route('session', [Crypt::encrypt($produto->id), 'boleto']) }}"  class="card-payment">
+                    <div class="img-method-payment">
+                        <i class="fa-brands fa-cc-stripe"></i>
                     </div>
 
-                    <div>
-                        <p style="font-size:14px; color:black">Comprar com Cartão</p>
-                    </div>
-                </a>
-
-                <a href="{{ route('session', [Crypt::encrypt($produto->id), 'card']) }}"
-                    style="
-                        width: 100%;
-                        height:80px;
-                        background-color: #fff;
-                        cursor:pointer;
-                        text-decoration:none;
-                        display: flex;
-                        align-items: center;
-                        border: 1px solid #e0e0e0;
-                        padding: 0 30px;
-                        gap: 20px;"
-                    >
-
-                    <div style="height: 50px;  width:50px; background-color:#e0e0e0; border-radius:50%; display:flex;  justify-content: center; align-items: center">
-                        <i class="fa-solid fa-credit-card" style="font-size: 24px;"></i>
-                    </div>
-
-                    <div>
-                        <p style="font-size:14px; color:black">Comprar com Cartão</p>
-                    </div>
-                </a>
-
-                <span style="margin-top: 20px; font-size:12px">Com outros meios de pagamento</span>
-
-
-                <a href="{{ route('session', [Crypt::encrypt($produto->id), 'boleto']) }}"
-                    style="
-                        width: 100%;
-                        height:80px;
-                        background-color: #fff;
-                        cursor:pointer;
-                        text-decoration:none;
-                        display: flex;
-                        align-items: center;
-                        border: 1px solid #e0e0e0;
-                        margin-top: 20px;
-                        padding: 0 30px;
-                        gap: 20px;"
-                    >
-
-                    <div style="height: 50px;  width:50px; background-color:#e0e0e0; border-radius:50%; display:flex;  justify-content: center; align-items: center">
-                        <i class="fa-brands fa-cc-stripe" style="font-size: 24px;"></i>
-                    </div>
-
-                    <div>
-                        <p style="font-size:14px; color:black">Boleto</p>
-                        <span style="font-size:12px; color:#aaa">Será aprovado em 1 a 2 dias</span>
+                    <div class="description-method-payment">
+                        <p>Boleto</p>
+                        <span>Será aprovado em 1 a 2 dias</span>
                     </div>
                 </a>
             </div>
 
-            <div style="
-                background-color: #f7f7f7;
-                width:350px; display:flex;
-                justify-content: center;
-                flex-direction: column;
-                padding: 60px 30px;
-                gap: 12px; "
-                >
-                <p style="font-size: 14px; font-weight: 600;">Resumo da compra</p>
+            <div class="purchase-details">
+                <p>Resumo da compra</p>
 
                 <hr>
-
                 <div>
-                    <div style="display: flex; justify-content: space-between;">
-                        <span style="font-size: 14px;">Produto</span>
-                        <span style="font-size: 14px;">R$ 259,00</span>
+                    <div class="section-info">
+                        <span>Produto</span>
+                        <span>R$ {{ number_format($produto->preco_desconto, 2, ',', '.') }}</span>
                     </div>
 
-                    <div style="display: flex; justify-content: space-between;">
-                        <span style="font-size: 14px;">Frete</span>
-                        <span style="font-size: 14px;">Grátis</span>
+                    <div class="section-info">
+                        <span>Frete</span>
+                        <span style="color: #00a650">Grátis</span>
                     </div>
                 </div>
-
                 <hr>
 
-                <div style="display: flex; justify-content: space-between;">
-                    <span style="font-size: 14px;">Você pagará</span>
-                    <span style="font-size: 14px; font-weight: 600">R$ 259,00</span>
+                <div class="section-info">
+                    <span>Você pagará</span>
+                    <span style="font-weight: 600">R$ {{ number_format($produto->preco_desconto, 2, ',', '.') }}</span>
                 </div>
-
-
             </div>
-
 
         </div>
     </main>
